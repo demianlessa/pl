e02ad9f972228d2511f52a9d6d45b16d76366a76
Demian Lessa <demian@lessa.org>
2016-12-25 16:11:49 -0500

    Adding more tests for Token.

--- lambda/src/test/java/org/lessa/lambda/TokenTest.java
+++ lambda/src/test/java/org/lessa/lambda/TokenTest.java
@@ -9,26 +9,42 @@ import org.testng.annotations.Test;
 
 public class TokenTest {
 
+   private static final Token TK_CLOSING_PARENS = new Token(TokenClass.CLOSING_PARENS, ")");
+   private static final Token TK_DEF = new Token(TokenClass.DEF, "def");
+   private static final Token TK_DOT = new Token(TokenClass.DOT, ".");
+   private static final Token TK_EQUALS = new Token(TokenClass.EQUALS, "=");
+   private static final Token TK_LAMBDA = new Token(TokenClass.LAMBDA, "λ");
+   private static final Token TK_NAME1 = new Token(TokenClass.NAME, "name1");
+   private static final Token TK_NAME2 = new Token(TokenClass.NAME, "name2");
+   private static final Token TK_OPENING_PARENS = new Token(TokenClass.OPENING_PARENS, "(");
+
    @Test
    public void given_distinct_tokens_all_are_added_to_map() {
-      
+
       final Map<Token, Integer> map = new HashMap<>();
-      map.put(new Token(TokenClass.CLOSING_PARENS, ")"), 1);
-      map.put(new Token(TokenClass.DEF, "def"), 2);
-      map.put(new Token(TokenClass.DOT, "dot"), 3);
-      map.put(new Token(TokenClass.EQUALS, "="), 4);
-      map.put(new Token(TokenClass.LAMBDA, "λ"), 5);
-      map.put(new Token(TokenClass.NAME, "name1"), 6);
-      map.put(new Token(TokenClass.NAME, "name2"), 7);
-      map.put(new Token(TokenClass.OPENING_PARENS, "("), 8);
+
+      map.put(TK_CLOSING_PARENS, 1);
+      map.put(TK_DEF, 2);
+      map.put(TK_DOT, 3);
+      map.put(TK_EQUALS, 4);
+      map.put(TK_LAMBDA, 5);
+      map.put(TK_NAME1, 6);
+      map.put(TK_NAME2, 7);
+      map.put(TK_OPENING_PARENS, 8);
 
       Assert.assertEquals(map.size(), 8);
+      Assert.assertEquals(map.get(TK_CLOSING_PARENS), new Integer(1));
+      Assert.assertEquals(map.get(TK_DEF), new Integer(2));
+      Assert.assertEquals(map.get(TK_DOT), new Integer(3));
+      Assert.assertEquals(map.get(TK_EQUALS), new Integer(4));
+      Assert.assertEquals(map.get(TK_LAMBDA), new Integer(5));
       Assert.assertEquals(map.get(new Token(TokenClass.NAME, "name1")), new Integer(6));
       Assert.assertEquals(map.get(new Token(TokenClass.NAME, "name2")), new Integer(7));
+      Assert.assertEquals(map.get(TK_OPENING_PARENS), new Integer(8));
    }
 
    @Test(dataProvider = "tokenPairs")
-   public void given_two_tokens_equals_should_return_expected(Token first, Token second,
+   public void given_two_tokens_equals_should_return_expected(Object first, Object second,
          boolean expectedEqual) {
       Assert.assertEquals(first.equals(second), expectedEqual);
    }
@@ -36,19 +52,19 @@ public class TokenTest {
    @DataProvider(name = "tokenPairs")
    private Object[][] createValidProgram() {
       return new Object[][] {
-         {
-            new Token(TokenClass.CLOSING_PARENS, ")"),
-            new Token(TokenClass.CLOSING_PARENS, ")"),
-            true },
-         { new Token(TokenClass.DEF, "def"), new Token(TokenClass.DEF, "def"), true },
-         { new Token(TokenClass.DOT, "."), new Token(TokenClass.DOT, "."), true },
-         { new Token(TokenClass.EQUALS, "="), new Token(TokenClass.EQUALS, "="), true },
-         { new Token(TokenClass.LAMBDA, "λ"), new Token(TokenClass.LAMBDA, "λ"), true },
-         { new Token(TokenClass.NAME, "name"), new Token(TokenClass.NAME, "name"), true },
-         { new Token(TokenClass.NAME, "name1"), new Token(TokenClass.NAME, "name2"), false },
-         {
-            new Token(TokenClass.OPENING_PARENS, "("),
-            new Token(TokenClass.OPENING_PARENS, "("),
-            true }, };
+         { TK_CLOSING_PARENS, TK_CLOSING_PARENS, true },
+         { TK_DEF, TK_DEF, true },
+         { TK_DOT, TK_DOT, true },
+         { TK_EQUALS, TK_EQUALS, true },
+         { TK_LAMBDA, TK_LAMBDA, true },
+         { TK_NAME1, TK_NAME1, true },
+         { TK_NAME2, TK_NAME2, true },
+         { TK_NAME1, TK_NAME2, false },
+         { TK_OPENING_PARENS, TK_OPENING_PARENS, true },
+         { TK_DEF, null, false },
+         { TK_DEF, new Token(TokenClass.DEF, "not_def"), false },
+         { TK_DEF, new Token(TokenClass.DEF, null), false },
+         { TK_DEF, new Token(TokenClass.NAME, "def"), false },
+         { TK_DEF, "def", false }, };
    }
 }
